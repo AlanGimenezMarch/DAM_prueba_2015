7180281f164263b23c3e4225b4fc0d88a7cfa18e
algima01 <algima01@A18b1.alumnado.flo>
2016-02-15 16:42:47 +0100

    Examen de Entornos de desarrollo segunda evaluación. Segunda parte: PR02

---
+++ ExamenEntornos/src/PR02.java
@@ -0,0 +1,33 @@
+
+public class PR02 {
+
+	public static void main(String[] args) {
+		//declaramos variables
+		String mensaje = "Paquito es el tio mas guapo que conozco";
+		String mensajeCodificado = "";
+		String temporal = "";
+		//convertimos mensaje a mayúsculas y lo guardamos en mensajeCodificado
+		mensajeCodificado = mensaje.toUpperCase();
+		//recorremos los carácteres de mensajeCodificado
+		for(int i=0; i<mensajeCodificado.length(); i++){
+			//si i+1 es igual a la longitud de mensajeCodificado
+			if(i+1 == mensajeCodificado.length()){
+				//temporal es igual a temporal + carácter en la posición i de mensajeCodificado
+				temporal = temporal + mensajeCodificado.charAt(i);
+			//si no	
+			}else{
+				//temporal es igual a temporal + carácter en la posición i+1 de en la posición i + carácter en la posición i de mensajeCodificado
+				temporal = temporal + mensajeCodificado.charAt(i+1) + mensajeCodificado.charAt(i);
+				
+			}
+			//sumamos 1 a la i
+			i=i+1;
+		}
+		
+		mensajeCodificado =	temporal;
+		//Imprime por pantalla el mensaje codificado
+		System.out.println(mensajeCodificado);
+		
+	}
+
+}
